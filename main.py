9b2a8cf1094b5a7a10e382ecc8bde7d67aa17acf
Alexandre Coelho <alexcoelho2000@outlook.pt>
2026-03-16 23:43:58 +0000

    resolve n_bands > 6 or < 4 bug

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from resistor_logic import calculate_resistor
 
 n_bands = int(input("Enter number of resistor bands: "))
+while n_bands < 4 or n_bands > 6:
+    n_bands = int(input("Enter number of resistor bands again: "))
+
 colors = input("Enter resistor colors: ").lower().split()
 
 value, tol, t_cf = calculate_resistor(colors, n_bands)
